f52f5d073830aa48779c455324df41e582ede652
Alessio Bex <beccati.alessio@educ.di.unito.it>
2017-03-22 10:46:37 +0100

    Folder with examples

---
+++ examples/nav_footer.php
@@ -0,0 +1,9 @@
+<?php
+	include 'laod.php';
+	display_header();
+?>
+<body>
+	<?php create_nav(['pages'=>['Nav and Footer'=>'nav_footer.php']])?>
+	<!-- Content of body -->
+	<?php create_footer(['pages'=>['Nav and Footer'=>'nav_footer.php']])?>
+</body>
